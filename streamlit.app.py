15cd72baa9cb6d40917eae724283107781a89de9
Ndukwe Daniel <125179701+Than-yel@users.noreply.github.com>
2023-09-09 04:09:59 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -22,7 +22,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Welcome!"):
+if prompt := st.chat_input("Talk to prophet ikuku. What's your grievance!"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
