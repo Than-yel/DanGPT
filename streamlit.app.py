a51ab52c900aefb0f15ba7390f379aff7fd20ff7
Ndukwe Daniel <125179701+Than-yel@users.noreply.github.com>
2023-09-09 04:04:44 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -4,7 +4,7 @@ import openai
 import streamlit as st
 
 with st.sidebar:
-    st.title('🤖💬 OpenAI Daniel')
+    st.title('🤖💬 Prophet Ikuku')
     if 'OPENAI_API_KEY' in st.secrets:
         st.success('API key already provided!', icon='✅')
         openai.api_key = st.secrets['OPENAI_API_KEY']
