3330e8860a55b7f2b3455e8ea0ec5eddf8805c4e
Ndukwe Daniel <125179701+Than-yel@users.noreply.github.com>
2026-01-06 20:48:30 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -4,7 +4,7 @@ import openai
 import streamlit as st
 
 with st.sidebar:
-    st.title('🤖💬 Prophet Ikuku')
+    st.title('🤖💬 Ndukwe Daniel')
     if 'OPENAI_API_KEY' in st.secrets:
         st.success('API key already provided!', icon='✅')
         openai.api_key = st.secrets['OPENAI_API_KEY']
@@ -22,7 +22,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Talk to prophet ikuku. What's your grievance!"):
+if prompt := st.chat_input("Tell me what you want from the job."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
